21761d3141dfbd2848d81a58a7473d5a52196fa2
Shashwat <92082891+shashwatkanungo@users.noreply.github.com>
2025-06-10 13:10:20 +0530

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 def load_and_clean_excel(uploaded_file):
-    df = pd.read_excel(uploaded_file)
-    df.columns = [col.strip().replace(" ", "_").lower() for col in df.columns]
+    df = pd.read_excel(uploaded_file)  # loading the excel file 
+    df.columns = [col.strip().replace(" ", "_").lower() for col in df.columns]   # cleaning the column names by replace spaces with underline, convert to lowercase and remove leading spaces
     return df
